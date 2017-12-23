8e41cd3b55ae0d82852074279db506ff8625a2c8
Greg Pratt <gpratt3151@yahoo.com>
2017-12-23 15:22:48 -0600

    Initial commit

---
+++ BatteryMINDer.ino
@@ -0,0 +1,77 @@
+/*
+ * Based on delay sketch & http://arduino-info.wikispaces.com/ArduinoPower#4-8
+ */
+
+const long oneSecond = 1000;  // a second is a thousand milliseconds
+const long tenSeconds = oneSecond * 5;
+const long oneMinute = oneSecond * 60;
+const long oneHour   = oneMinute * 60;
+const long oneDay    = oneHour * 24;
+
+/*-----( Import needed libraries )-----*/
+/*-----( Declare Constants )-----*/
+#define RELAY_ON 0
+#define RELAY_OFF 1
+/*-----( Declare objects )-----*/
+/*-----( Declare Variables )-----*/
+#define Relay_1  2  // Arduino Digital I/O pin number
+#define Relay_2  12
+#define Relay_3  8
+#define Relay_4  7
+
+void setup()
+{
+  //-------( Initialize Pins so relays are inactive at reset)----
+  digitalWrite(Relay_1, RELAY_OFF);
+  digitalWrite(Relay_2, RELAY_OFF);
+  digitalWrite(Relay_3, RELAY_OFF);
+  digitalWrite(Relay_4, RELAY_OFF);
+
+  //---( THEN set pins as outputs )---- 
+  pinMode(Relay_1, OUTPUT);
+  pinMode(Relay_2, OUTPUT);
+  pinMode(Relay_3, OUTPUT);
+  pinMode(Relay_4, OUTPUT);
+  delay(4000); //Check that all relays are inactive at Reset
+  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println("Set Relay #1 On");
+  digitalWrite(Relay_1, RELAY_ON);
+  Serial.println("On for 1 Hour");
+  delay(oneHour);
+  Serial.println("Set Relay #1 Off");
+  digitalWrite(Relay_1, RELAY_OFF);
+  Serial.println("Delay 1 second");
+  delay(oneSecond);
+  
+  Serial.println("Set Relay #2 On");
+  digitalWrite(Relay_2, RELAY_ON);
+  Serial.println("On for 1 Hour");
+  delay(oneHour);
+  Serial.println("Set Relay #2 Off");
+  digitalWrite(Relay_2, RELAY_OFF);
+  Serial.println("Delay 1 second");
+  delay(oneSecond);
+
+  Serial.println("Set Relay #3 On");
+  digitalWrite(Relay_3, RELAY_ON);
+  Serial.println("On for 10 seconds");
+  delay(tenSeconds);
+  Serial.println("Set Relay #3 Off");
+  digitalWrite(Relay_3, RELAY_OFF);
+  Serial.println("Delay 1 second");
+  delay(oneSecond);
+
+  Serial.println("Set Relay #4 On");
+  digitalWrite(Relay_4, RELAY_ON);
+  Serial.println("On for 10 seconds");
+  delay(tenSeconds);
+  Serial.println("Set Relay #4 Off");
+  digitalWrite(Relay_4, RELAY_OFF);
+  Serial.println("Delay 1 second");
+  delay(oneSecond);
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # BatteryMINDer
 Enable BatteryMINDer to charge individual batteries by switching between them in sequence
+
+## Purpose
+I have a single [BatteryMINDer(r)](http://www.batteryminders.com/batteryminder-plus-charger-model-12117-12v-1-33-amp-charger-maintainer-desulfator/) and 4 lead acid batteries. It's not recommended to connect the batteries in parallel if they are not all the same capacity. The idea is to allow the charger to switch between batteries in sequence. Because this requires physical contacts (i.e. relays) the time between switching should be frequent enough not to let the battery sulfate, but infrequent enough to not wear out the relays as they do have a limit to the number of times they switch.
+
+## Solution
+Using an Arduino or clone and 4 relays I have the ability to control the relays and create a custom program based upon my needs.
+ 
+## Approach
+Use ideas, principles, and concepts from DevOps develop a minimum viable product to meet my needs and as I learn (via feedback) add features as necessary.
+
+### First iteration
+__Goal__
+Control 2 relays
+Use delay() to control the time between sequences
+Write the state out to the serial port
+
+__Outcomes__ 
+Learn how relays are controlled (i.e. whether pinmode has to be HIGH or LOW)
+Learn how to connect the relays to the digital ports
+Initialize the relays so they are off first
+Sequence between them every hour
+
